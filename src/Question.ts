e6b40d2ae522fd89eaa1bb312778dd1897c12cc6
kaiba <kaibadash@gmail.com>
2024-02-25 10:12:05 +0900

    Add files

---
+++ src/Question.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+import { Item } from "./Item";
+
+export class Question {
+  public text: string = "";
+  public items: Item[];
+
+  constructor(text: string) {
+    this.items = [];
+    this.text = text;
+  }
+
+  add(item: Item) {
+    this.items.push(item);
+  }
+}
