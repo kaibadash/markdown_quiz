bff9fa5926236251e351b9de9ef6f5eb1dd5f5f4
kaiba <kaibadash@gmail.com>
2024-02-25 13:15:43 +0900

    Disable checkbox if ended

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -92,16 +92,13 @@ const Quiz: React.FC = () => {
               return (
                 <li key={item.id} className="list-group-item">
                   <label className="form-check-label">
-                    {questions.ended ? (
-                      ""
-                    ) : (
-                      <input
-                        type="checkbox"
-                        checked={item.selected}
-                        onChange={(e) => handleCheckboxChange(item)}
-                        className="form-check-input me-1"
-                      />
-                    )}
+                    <input
+                      type="checkbox"
+                      checked={item.selected}
+                      onChange={(e) => handleCheckboxChange(item)}
+                      disabled={questions.ended}
+                      className="form-check-input me-1"
+                    />
                     {questions.ended ? (
                       item.correct ? (
                         <span className="answer">[Answer]</span>
